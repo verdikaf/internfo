69b96f8297abc8e4c78af85864bd7831d4f7cbb7
galangveda-31 <61674460+galangveda-31@users.noreply.github.com>
2020-04-30 18:59:02 +0700

    WEB

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// LOGIN
 Route::get('/login', 'LoginController@index');
 Route::get('/signup', 'LoginController@signup');
+Route::get('/signup/perusahaan', 'LoginController@signupPerusahaan');
 
+// DASHBOAORD
 Route::get('/dashboard/pemagang', 'DashboardController@pemagang');
 Route::get('/dashboard/admin', 'DashboardController@admin');
+
+// CRUD PERUSAHAAN --
+Route::get('/admin/perusahaan', 'PerusahaanController@index');
+Route::get('/admin/perusahaan/add', 'PerusahaanController@add');
+Route::get('/admin/perusahaan/add/proses', 'PerusahaanController@addProses');
+Route::get('/admin/perusahaan/edit/{id}', 'PerusahaanController@edit');
+Route::get('/admin/perusahaan/edit/proses', 'PerusahaanController@editProses');
