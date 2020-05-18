3ba53a917d2b7ade5e2cce14c9bde6b87f3dba7a
galangveda-31 <61674460+galangveda-31@users.noreply.github.com>
2020-05-18 14:18:40 +0700

    AboutUS
    Mengembalikan File About Us yang sempat error karena masalah GitHub

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -1,78 +1,111 @@
-<!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{url('/assets/library/bootstrap/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{url('/assets/library/fontawesome/css/fontawesome.css')}}">
+    <link rel="stylesheet" href="{{url('/assets/library/extras/dashboard.css')}}">
+    <link rel="stylesheet" href="{{url('/assets/css/sidebar.css')}}">
+    <title>INTERNFO</title>
+</head>
+<body class="bodyv2">
 
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Internfo</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="apple-touch-icon" href="{{url('apple-touch-icon.png')}}">
+      <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-light fixed-top">
+    <div class="container">
+        <img src="{{url('/assets/images/logo/Inferno_Type_whi.png')}}" alt="" class="headlogo" style="">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a href="{{url('/login')}}" type="button" class="btn btn-logout">Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Header -->
+  <header class="bg-primarys2 py-5 mb-5">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center">
+        <div class="col-lg-12">
+            <center>
+                <p class="subtitle2" style="padding-bottom:0px">Welcome to</p><br>
+                <img src="{{url('/assets/images/logo/Inferno_type_whi.png')}}" alt="" class="titleboutus">        
+            </center>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Page Content -->
+  <div class="container-fluid wrapaboutus">
+    <div class="row">
+      <div class="col-md-6 mb-5">
+            <p class="sidetitle-aboutus">Tujuan Kami</p>
+            <br>
+            <p class="sidesubtitle-aboutus">Tujuan kami hadir disini adalah untuk mempermudah para 
+                mahasiswa yang kesulitan mencari tempat yang memang menyediakan program magang</p>
+            <br>
+      </div>
+      <div class="col-md-6 mb-5">
+            <p class="sidetitle-aboutus">Fokus Kami</p>
+            <br>
+            <p class="sidesubtitle-aboutus">Terus melakukan pengembangan inovasi agar mempermudah 
+              masyarakat dalam melakukan kegiatan</p>
+            <br>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid wrapaboutus2">
+  <p class="sidetitle-aboutus" style="margin-bottom:50px">Team Kami</p>
+    <div class="row">
+      <div class="col-md-4 mb-5">
+      <center>
+            <img src="assets/images/logo/Asset 2.png" class="circular-square" alt="">
+            <p class="sideteam-aboutus">Verdika Fajar S.</p>
+      </center>
+            
+            <br>
+      </div>
+      <div class="col-md-4 mb-5">
+      <center>
+            <img src="assets/images/logo/Asset 2.png" class="circular-square" alt="">
+            <p class="sideteam-aboutus">Dyah Ayu P.</p>
+      </center>
+      </div>
+      <div class="col-md-4 mb-5">
+      <center>
+            <img src="assets/images/logo/Asset 2.png" class="circular-square" alt="">
+            <p class="sideteam-aboutus">Galang indra V.</p>
+      </center>
+      </div>
+    </div>
+  </div>
+  <!-- /.container -->
+
+  
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Internfo 2020</p>
+    </div>
+    <!-- /.container -->
+  </footer>
 
 
-        <link rel="stylesheet" href="{{url('assets/css/fonticons.css')}}">
-        <link rel="stylesheet" href="{{url('assets/fonts/stylesheet.css')}}">
-        <link rel="stylesheet" href="{{url('assets/css/font-awesome.min.css')}}">
-        <link rel="stylesheet" href="{{url('assets/css/bootstrap.min.css')}}">
-        <link rel="stylesheet" href="{{url('assets/extras/crud.css')}}">
 
-
-        <!--For Plugins external css-->
-        <link rel="stylesheet" href="{{url('assets/css/plugins.css')}}" />
-
-        <!--Theme custom css -->
-        <link rel="stylesheet" href="{{url('assets/css/style.css')}}">
-
-        <!--Theme Responsive css-->
-        <link rel="stylesheet" href="{{url('assets/css/responsive.css')}}" />
-
-        <script src="{{url('assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js')}}"></script>
-
-        <!-- <link rel="stylesheet" href="{{url('/assets/library/bootstrap-4.4.1/css/bootstrap.css')}}"> -->
-        <!-- <link rel="stylesheet" href="{{url('/assets/library/fontawesome-5.13.0/css/fontawesome.css')}}">
-        <link rel="stylesheet" href="{{url('/assets/library/fontawesome-5.13.0/css/solid.css')}}">
-        <link rel="stylesheet" href="{{url('/assets/style.css')}}"> -->
-    
-    </head>
-    <body data-spy="scroll" data-target=".navbar-collapse">
-        <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-
-        @include('header_pemagang')
-    <div id="big_container">
-        <section id="home" class="home" >
-            <div class="overlay">
-                <div class="container">
-                    <div class="row">
-                        
-                        
-                    <center><div class="col-sm-6">
-                            <p class="titles">Internfo</p>
-                        </div></center>
-                        
-
-
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        @include('footer_perusahaan')
-
-
-        <script src="{{url('assets/js/vendor/jquery-1.11.2.min.js')}}"></script>
-        <script src="{{url('assets/js/vendor/bootstrap.min.js')}}"></script>
-
-        <script src="{{url('assets/js/jquery.easypiechart.min.js')}}"></script>
-        <script src="{{url('assets/js/jquery.mixitup.min.js')}}"></script>
-        <script src="{{url('assets/js/jquery.easing.1.3.js')}}"></script>
-
-        <script src="{{url('assets/js/plugins.js')}}"></script>
-        <script src="{{url('assets/js/main.js')}}"></script>
-</div>
-    </body>
+</body>
 </html>
